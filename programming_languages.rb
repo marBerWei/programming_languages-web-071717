097f8f07183d3719ef11fa50fb7d7083144ed38c
Martina Weidenbaum <martina.weidenbaum@gmail.com>
2017-07-11 15:42:54 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  array = []
+  languages.each do |oo_func, lang_hash|
+    array << lang_hash.keys
+    new = array.flatten
+    duplicates = new.select { |e| new.count(e) > 1 }
+      lang_hash.each do |language, type|
+        new_hash[language] = {:type=>type[:type],:style=>[oo_func]}
+          if duplicates.uniq[0] == language
+            new_hash[language] = {:type=>type[:type],:style=>[:functional, :oo]}
+          end
+        end
+      end
+
+  new_hash
 end
